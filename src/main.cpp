4302ee3a5fe2a77fc96f3e495869c184f7ac8077
SatosiNakamoto <kotovsshikov31415926@gmail.com>
2021-02-17 19:07:28 +0300

    added simple threading

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,13 @@
 #include "roiSelector.h"
 #include "tracker.h"
 
+
 void run()
 {
 	cv::VideoCapture cap;
 	cv::VideoCapture capitan;
 	cap.open(0);
-	const int roiCounter = 1;
+	const int roiCounter = 2;
 	roiSelector box(roiCounter);
 	tracker track[roiCounter];
 	cv::VideoWriter save;
@@ -21,26 +22,44 @@ void run()
 	bool init = true;
 	cv::Rect** roi;
 	std::string trackingWindow = "tracking.jpg";
+
+	while(init)
+	{
+		cap.read(frame);
+		roi = box.add(trackingWindow, frame);
+		for (int i = 0; i < roiCounter; ++i)
+			track[i].initTracker(*roi[i], frame);
+		init = !init;
+	}
+
 	while(cap.read(frame))
 	{
-		if(init)
+		std::thread* threadpool = new std::thread[roiCounter];
+
+		for (int i = 0; i < roiCounter; ++i)
 		{
-			roi = box.add(trackingWindow, frame);
-			for(int i = 0; i < roiCounter; ++i)
-				track[i].initTracker(*roi[i], frame);
-			init = !init;
-		}
-		else
-			for (int i = 0; i < roiCounter; ++i)
-			{
+			threadpool[i] = std::thread([roi, &track, frame, i, trackingWindow] {
 				*roi[i] = track[i].update(frame);
 				cv::rectangle(frame, *roi[i], cv::Scalar(255, 255, 0));
 				cv::imshow(trackingWindow, frame);
-				save << frame;
-			}
+
+				});
+			//save << frame;
+		}
+
+		for (int i = 0; i < roiCounter; ++i)
+			threadpool[i].join();
 		cv::waitKey(20);
 	}
 }
+
+/*
+	TODO:	bug with rect. exception when rectangle is outside window scope
+			make threading
+			create threadpool class
+			create own wrappers of memmory control functions (malloc calloc etc)
+			review - look for the bugs
+*/
 
 int main()
 {
